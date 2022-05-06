674302dff763a76506f9b571ce79c5ca25dbb9e7
Veronica <zeballos.veronica@gmail.com>
2022-05-05 21:40:13 -0400

    Spring Boot Demo part3

--- Spring-04-Abstraction/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
@@ -1,9 +1,11 @@
 package com.cydeo.proxy;
 
 import com.cydeo.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("PUSH")
 public class CommentPushNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment){
--- Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -3,6 +3,8 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +13,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH")CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
