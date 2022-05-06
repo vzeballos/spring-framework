cefbf3e4b78a646d59512640842f11326ed8a132
Veronica <zeballos.veronica@gmail.com>
2022-05-05 23:21:15 -0400

    Spring Boot Demo part4

--- Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -5,20 +5,11 @@ import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentService {
 
-//    @Value("${name}")
-//    private String x;
-//
-//    @Value("${server.port}")
-//    private Integer x;
-//
-//    @Value("${days}")
-//    private String[] days;
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
@@ -26,10 +17,9 @@ public class CommentService {
     public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
-        System.out.println("Hello");
     }
 
-    public void publishComment (Comment comment){
+    public void publishComment(Comment comment){
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
     }
