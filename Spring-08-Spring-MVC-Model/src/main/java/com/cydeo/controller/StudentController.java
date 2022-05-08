6ee67785f835d333f86079cfda264e9ef5790cdc
Veronica <zeballos.veronica@gmail.com>
2022-05-08 19:40:00 -0400

    Spring MVC - model part2

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -12,6 +16,21 @@ public class StudentController {
 
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course","MVC");
+
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId(0-1000) and show it in your UI
+        int studentId = new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        model.addAttribute("numbers", numbers);
 
         return "/student/welcome";
     }
