8abc314d70151e56df45e00536b97dd7aaa63e3c
Veronica <zeballos.veronica@gmail.com>
2022-05-10 23:18:09 -0400

    Spring Thymeleaf

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/Spring09SpringThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09SpringThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09SpringThymeleafApplication.class, args);
+    }
+}
