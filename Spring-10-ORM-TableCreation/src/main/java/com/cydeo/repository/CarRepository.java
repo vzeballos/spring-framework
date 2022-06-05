4d887224d98d06a8a5f6f67566c9bd0be890588e
Veronica <zeballos.veronica@gmail.com>
2022-06-05 19:14:36 -0400

    repository

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -0,0 +1,13 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository  extends JpaRepository<Car,Long> {
+
+    //custom methods
+    //sql
+
+}
