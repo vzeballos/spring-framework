886c0ed22a603f07409ec89391abf0ecfbfd9f22
Veronica <zeballos.veronica@gmail.com>
2022-06-05 18:55:01 -0400

    inheritance

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -1,13 +1,17 @@
 package com.cydeo.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Employee { //employee
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
 
 
