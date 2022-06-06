6bb487be10ac68d1323e7f8f1b2a05b7300738c8
Veronica <zeballos.veronica@gmail.com>
2022-06-05 20:05:45 -0400

    task

---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "employees")
+@Data
+@NoArgsConstructor
+public class Employee extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+    private int salary;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, int salary, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.salary = salary;
+        this.gender = gender;
+    }
+}
---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/resources/departments.sql
@@ -0,0 +1,5 @@
+create table departments (
+    department varchar(100),
+    division varchar(100),
+    primary key (department)
+);
