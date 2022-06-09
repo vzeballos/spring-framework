5a0e858bfcba4948644c86554b331f380648cc6b
Veronica <zeballos.veronica@gmail.com>
2022-06-08 20:03:50 -0400

    one to one - payments and payment details

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/model/PaymentDetail.java
@@ -0,0 +1,36 @@
+package com.cydeo.model;
+
+import com.cydeo.enums.Status;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "paymentDetails")
+@Data
+@NoArgsConstructor
+public class PaymentDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal merchantPaymentAmount;
+    private BigDecimal commissionAmount;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate payoutDate;
+
+    @OneToOne(mappedBy = "paymentDetail", cascade = CascadeType.ALL)
+    @JoinColumn(name = "payment_id")
+    private Payment payment;
+
+    public PaymentDetail(BigDecimal merchantPaymentAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
+        this.merchantPaymentAmount = merchantPaymentAmount;
+        this.commissionAmount = commissionAmount;
+        this.payoutDate = payoutDate;
+    }
+}
