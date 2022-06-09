331f90479a5fc96473be5d6747a4e2ad1494275e
Veronica <zeballos.veronica@gmail.com>
2022-06-08 21:53:03 -0400

    many to many - item and cart

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Item.java
@@ -0,0 +1,30 @@
+package com.cydeo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name= "items")
+@Data
+@NoArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToMany(mappedBy = "itemList")
+    private List<Cart> cartList;
+
+    public Item(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+}
