bfeffd86b24d0f559029c205b197ba32218822d3
Veronica <zeballos.veronica@gmail.com>
2022-06-12 18:23:24 -0400

    refactor and course queries

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,51 +1,87 @@
-package com.cydeo.bootstrap;
+package com.cydeo.boostrap;
 
+import com.cydeo.repository.CourseRepository;
 import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
-
     private final DepartmentRepository departmentRepository;
-
     private final EmployeeRepository employeeRepository;
+    private final CourseRepository courseRepository;
 
-    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository){
-        this.regionRepository = regionRepository;
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository, CourseRepository courseRepository) {this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
+        this.courseRepository = courseRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception{
+    @Transactional
+    public void run(String... args) throws Exception {
+        System.out.println("-----------------REGION START-----------------");
 
-        System.out.println("-----------------Region Start-----------------");
-        //how will I access to method that I just built?
-        System.out.println("findByCountry: " + regionRepository.findByCountry("Canada"));
-        System.out.println("findDistinctByCountry: " + regionRepository.findDistinctByCountry("Canada"));
-        System.out.println("findByCountryContaining: " + regionRepository.findByCountryContaining("United"));
-        System.out.println("findByCountryContainingOrderByCountry: " + regionRepository.findByCountryContainingOrderByCountry("Asia"));
-        System.out.println("findTop2ByCountry: " + regionRepository.findTop2ByCountry("Canada"));
-        System.out.println("-----------------Region End-----------------");
+        System.out.println("findByCountry:" + regionRepository.findByCountry("Canada"));
+        System.out.println("findDistinctByCountry:" + regionRepository.findDistinctByCountry("Canada"));
+        System.out.println("findByCountryContaining:" + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainingOrderByCountry:" + regionRepository.findByCountryContainingOrderByCountry("Asia"));
+        System.out.println("findTopByCountry:" + regionRepository.findTop2ByCountry("Canada"));
 
-        System.out.println("");
+        System.out.println("-----------------REGION END-----------------");
 
-        System.out.println("-----------------Department Start-----------------");
-        System.out.println("findByDepartment: " + departmentRepository.findByDepartment("Furniture"));
-        System.out.println("findByDivisionIs: " + departmentRepository.findByDivisionIs("Outdoors"));
-        System.out.println("findDistinctTop3ByDivisionContains: " + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
-        System.out.println("-----------------Department End-----------------");
+        System.out.println("-----------------DEPARTMENT START-----------------");
 
-        System.out.println("");
+        System.out.println("findByDepartment:" + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivisionIs:" + departmentRepository.findByDivisionIs("Outdoors"));
+        System.out.println("findDistinctTop3ByDivisionContains:" + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
 
-        System.out.println("-----------------Employee Start-----------------");
-        System.out.println("getEmployeeDetail: " + employeeRepository.getEmployeeDetail());
-        System.out.println("getEmployeeSalary: " + employeeRepository.getEmployeeSalary());
-        System.out.println("-----------------Employee End-----------------");
+
+
+        System.out.println("-----------------DEPARTMENT END-----------------");
+
+        System.out.println("-----------------EMPLOYEE START-----------------");
+
+        System.out.println("getEmployeeDetail:" + employeeRepository.getEmployeeDetail());
+        System.out.println("getEmployeeSalary:" + employeeRepository.getEmployeeSalary());
+
+
+
+        System.out.println("-----------------EMPLOYEE  END-----------------");
+
+        System.out.println("-----------------COURSE START-----------------");
+
+        courseRepository.findByCategory("Spring").forEach(System.out::println);
+
+        System.out.println("---------------------------");
+
+        courseRepository.findByCategoryOrderByName("Spring").forEach(System.out::println);
+
+        System.out.println("---------------------------");
+
+        System.out.println(courseRepository.existsByName("JavaScript for All"));
+
+        System.out.println("---------------------------");
+
+        System.out.println(courseRepository.countByCategory("Spring"));
+
+        System.out.println("---------------------------");
+
+        courseRepository.findByNameStartsWith("Scalable").forEach(System.out::println);
+
+        System.out.println("---------------------------");
+
+        courseRepository.streamByCategory("Spring").forEach(System.out::println);
+
+
+
+        System.out.println("-----------------COURSE  END-----------------");
+
+
     }
 }
