b2b88b1c0d7513d2ec46cb14f342e7d3adb7dc3d
Veronica <zeballos.veronica@gmail.com>
2022-06-22 22:08:32 -0400

    genre repo

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -2,15 +2,22 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GenreRepository extends JpaRepository<Genre,Long> {
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that return all genres
-
+    @Query("SELECT g from Genre g")
+    List<Genre> fetchAll();
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns genres by containing name
+    @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%', ?1, '%')", nativeQuery = true)
+    List<Genre> retrieveByName(@Param("name") String name);
 }
