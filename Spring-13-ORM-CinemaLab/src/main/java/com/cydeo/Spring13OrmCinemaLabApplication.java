b239ab40de33042bf7fe71d90ac1603aef967f32
Veronica <zeballos.veronica@gmail.com>
2022-06-22 21:21:13 -0400

    Account repo

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/Spring13OrmCinemaLabApplication.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/Spring13OrmCinemaLabApplication.java
@@ -2,7 +2,6 @@ package com.cydeo;
 
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.output.MigrateResult;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,36 @@
+package com.cydeo.bootstrap;
+
+import com.cydeo.repository.*;
+import org.springframework.boot.CommandLineRunner;
+
+public class DataGenerator implements CommandLineRunner {
+
+    private final AccountRepository accountRepository;
+    private final CinemaRepository cinemaRepository;
+    private final MovieRepository movieRepository;
+    private final MovieCinemaRepository movieCinemaRepository;
+    private final TicketRepository ticketRepository;
+
+    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, MovieRepository movieRepository, MovieCinemaRepository movieCinemaRepository, TicketRepository ticketRepository) {
+        this.accountRepository = accountRepository;
+        this.cinemaRepository = cinemaRepository;
+        this.movieRepository = movieRepository;
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.ticketRepository = ticketRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println(accountRepository.fetchAdminUsers());
+        System.out.println(cinemaRepository.distinctBySponsoredName());
+        System.out.println(movieRepository.fetchAllMovieNames());
+        System.out.println(movieCinemaRepository.countAllByCinemaId(4L));
+        System.out.println(movieCinemaRepository.retrieveAllByLocationName("AMC Empire 25"));
+        System.out.println(ticketRepository.fetchAllTicketsByUserAccount(4L));
+        System.out.println(ticketRepository.fetchAllTicketsWithRangeOfDates(LocalDateTime.now().minusDays(25), LocalDateTime.now()));
+        System.out.println(ticketRepository.retrieveAllBySearchCriteria("it"));
+
+    }
+
+}
